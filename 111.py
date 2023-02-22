6e4c826048823177cfdcb338ebace5e3a5221ad1
Pasha <nikolaevpasa4@gmail.com>
2023-02-22 22:50:14 +0200

    first commit

---
+++ 111.py
@@ -0,0 +1,10 @@
+def Different():
+    liqst = [1,3,5,6,7,8,9,6,4,4]
+    a = len(liqst)
+    max1 = max(liqst) 
+    min1 = min(liqst)
+    result = max1 - min1
+    return result
+print(Different())
+
+
